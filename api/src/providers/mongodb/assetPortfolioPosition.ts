db7abfc4b36385d72ce6390c1e38672ae2d8874d
Allan Mariano <genivalallan@hotmail.com.br>
2022-05-21 00:58:36 -0300

    Add tickerName and tickerRegion fields to AssetPortfolioPosition

--- api/src/providers/mongodb/assetPortfolioPosition.ts
+++ api/src/providers/mongodb/assetPortfolioPosition.ts
@@ -3,6 +3,8 @@ import { ObjectId } from "mongodb";
 export type AssetPortfolioPosition = {
   _id: ObjectId;
   tickerSymbol: string;
+  tickerName: String;
+  tickerRegion: String;
   numberOfShares: number;
   createdAt: Date;
   lastUpdatedAt: Date | null;
