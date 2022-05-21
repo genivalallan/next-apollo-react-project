48c1d19da582c1ed9bb56e4efa4e00fd32a94406
Allan Mariano <genivalallan@hotmail.com.br>
2022-05-21 01:06:30 -0300

    Create Date GraphQL scalar resolver

--- api/src/graphql/resolvers.ts
+++ api/src/graphql/resolvers.ts
@@ -1,6 +1,28 @@
+import { GraphQLScalarType, Kind } from "graphql";
 import { DataSources, SearchArgs } from "./types";
 
+const dateScalar = new GraphQLScalarType<Date | null, number>({
+  name: "Date",
+  description: "Date custom scalar type",
+  serialize(value) {
+    return (value as Date).getTime(); // Convert outgoing Date to integer for JSON
+  },
+
+  parseValue(value) {
+    return new Date(value as number); // Convert incoming integer to Date
+  },
+
+  parseLiteral(ast) {
+    if (ast.kind === Kind.INT) {
+      return new Date(parseInt(ast.value, 10)); // Convert hard-coded AST string to integer and then to Date
+    }
+
+    return null; // Invalid hard-coded value (not an integer)
+  },
+});
+
 const resolvers = {
+  Date: dateScalar,
   Query: {
     search: async (
       _: any,
