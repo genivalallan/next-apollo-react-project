60c64ab7e76bc9b59236196609cbbd68beba2451
Allan Mariano <genivalallan@hotmail.com.br>
2022-06-10 14:43:29 -0300

    Apply styles to Loading

--- app/components/loading.tsx
+++ app/components/loading.tsx
@@ -1,9 +1,9 @@
-import styles from "./loading.module.css";
-
 const Loading: React.FC = () => {
   return (
-    <div className={styles.loadingBox}>
-      <p className={styles.loadingText}>&#10227; Carregando...</p>
+    <div className="w-1/2 mx-auto my-5 rounded-lg bg-gray-100 ">
+      <p className="w-full py-2 text-center text-slate-500 text-sm font-source-code-pro font-bold drop-shadow-md">
+        &#10227; Carregando...
+      </p>
     </div>
   );
 };
